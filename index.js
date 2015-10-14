cf4e0e0481f771c7ea4d1fd525eb4cbcb226ae70
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2015-10-14 10:50:02 +0200

    First working code

---
+++ index.js
@@ -0,0 +1,152 @@
+'use strict';
+
+const config = require('./config');
+const googleSpreadsheets = require('google-spreadsheets');
+const Slack = require('slack-client');
+
+const readline = require('readline');
+
+const google = require('googleapis');
+const OAuth2Client = google.auth.OAuth2;
+
+const dayOfWeekIdx = ((new Date()).getDay() - 1) % 7; // monday = 0
+
+const oauth2Client = new OAuth2Client(config.oauthClientId, config.oauthClientSecret, config.oauthRedirectUrl);
+
+const slack = new Slack(config.slackToken, true, true);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const lunches = {};
+
+
+function getOAuth2Token (client, callback) {
+    // generate consent page url
+    var url = client.generateAuthUrl({
+        // will return a refresh token
+        access_type: 'offline', // eslint-disable-line camelcase
+        // can be a space-delimited string or an array of scopes
+        scope: 'https://www.googleapis.com/auth/drive'
+    });
+
+    console.log('1. Visit the url:', url);
+    console.log('2. You will end up at ' + config.oauthRedirectUrl + '?code=<CODE>#');
+    rl.question('Enter the code here: ', (code) => {
+        // request access token
+        client.getToken(code, (err, tokens) => {
+            if (err) { throw err; }
+            // set tokens to the client
+            // TODO: tokens should be set by OAuth2 client.
+            client.setCredentials(tokens);
+            callback();
+        });
+    });
+}
+
+
+slack.on('message', event => {
+    var user, channel;
+    var isFemale;
+    var message = 'message' === event.type && event.text;
+    var response;
+    var lunch;
+
+    console.log('Got', message);
+
+    user = slack.getUserByID(event.user);
+    channel = slack.getChannelGroupOrDMByID(event.channel);
+
+    lunch = lunches[user.name];
+    response = '@' + user.name + ': ';
+
+
+    if (/co .*na (obiad|lunch|lancz)/i.test(message))
+    {
+        isFemale = user.name.split('.')[0].slice(-1);
+
+        if (lunch === undefined)
+        {
+            channel.send(response + 'Nie wiem kim jesteś, sorry.');
+        }
+        else if (!lunch || '-' === lunch)
+        {
+            if (isFemale)
+            {
+                channel.send(response + 'Wygląda na to, że nic dzisiaj nie zamówiłaś');
+            }
+            else
+            {
+                channel.send(response + 'Wygląda na to, że nic dzisiaj nie zamówiłeś');
+            }
+        }
+        else
+        {
+            channel.send(response + lunches[user.name]);
+        }
+    }
+
+    if (/what('s|is) for (dinner|lunch)/i.test(message))
+    {
+        isFemale = user.name.split('.')[0].slice(-1);
+
+        if (lunch === undefined)
+        {
+            channel.send(response + "I don't know who you are, sorry.");
+        }
+        else if (!lunch || '-' === lunch)
+        {
+            channel.send(response + 'It seems that you did not order anything today.');
+        }
+        else
+        {
+            channel.send(response + lunch);
+        }
+    }
+});
+
+slack.on('open', () => {
+    var channelNames = [];
+    var i;
+    for (i in slack.channels)
+    {
+        if (slack.channels[i].is_member)
+        {
+            channelNames.push('#' + slack.channels[i].name);
+        }
+    }
+    console.log('Ready to conversate in ' + channelNames.join(', '));
+});
+
+slack.on('error', error => {
+    console.log(error);
+});
+
+getOAuth2Token(oauth2Client, () => {
+    googleSpreadsheets({
+        key: config.lunchSpreadsheetId,
+        auth: oauth2Client
+    }, (googleSpreadsheetsErr, spreadsheet) => {
+        if (googleSpreadsheetsErr) { throw googleSpreadsheetsErr; }
+        spreadsheet.worksheets[config.lunchSheetIndex].cells({
+            range: config.lunchCellsRange
+        }, (err, data) => {
+            var i;
+            if (err)
+            {
+                throw err;
+            }
+
+            for (i in data.cells[5])
+            {
+                lunches[data.cells[5][i].value] = data.cells[dayOfWeekIdx + 6][i].value;
+            }
+            console.log('Lunches:', lunches);
+
+            slack.login();
+        });
+
+    });
+});
