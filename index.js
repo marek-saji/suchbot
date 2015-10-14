a9fa58e3f2b717d7d937ac57fc1c60b9e43c5db9
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2015-10-15 00:59:51 +0200

    Promisify

--- index.js
+++ index.js
@@ -16,54 +16,198 @@ const oauth2Client = new OAuth2Client(config.oauthClientId, config.oauthClientSe
 
 const slack = new Slack(config.slackToken, true, true);
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+var rl;
 
-const lunches = {
+const lunchCache = {
     timestamp: null,
     items: new Map()
 };
 
 
-function getOAuth2Token (client, callback) {
-    // generate consent page url
-    var url;
+function connectToDrive (client)
+{
+    return new Promise((resolve, reject) => {
+        if (config.oauth2Token)
+        {
+            client.setCredentials({
+                /* eslint-disable camelcase */
+                access_token: config.oauth2Token
+                /* eslint-enable camelcase */
+            });
+            resolve(oauth2Client);
+        }
+        else
+        {
+            let url = client.generateAuthUrl({
+                /* eslint-disable camelcase */
+                access_type: 'offline',
+                scope: [ 'https://www.googleapis.com/auth/drive' ]
+                /* eslint-enable camelcase */
+            });
+            console.log('1. Visit the url:', url);
+            console.log('2. You will end up at ' + config.oauthRedirectUrl + '?code=<CODE>#');
+            rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+            rl.question('Paste the code here: ', (code) => {
+                rl.close();
+                client.getToken(code, (err, tokens) => {
+                    if (err)
+                    {
+                        reject(err);
+                    }
+                    else
+                    {
+                        client.setCredentials(tokens);
+                        config.oauth2Token = tokens.access_token;
+                        fs.writeFile(
+                            './config.json',
+                            JSON.stringify(config, false, 4)
+                        );
+                        resolve(client);
+                    }
+                });
+            });
+        }
+    });
+}
 
-    if (config.oauth2Token)
-    {
-        client.setCredentials({
-            /* eslint-disable camelcase */
-            access_token: config.oauth2Token
-            /* eslint-enable camelcase */
+function connectToSpreadsheets (client)
+{
+    return new Promise((resolve, reject) => {
+        googleSpreadsheets(
+            {
+                key: config.lunchSpreadsheetId,
+                auth: client
+            },
+            (err, spreadsheet) => {
+                if (err)
+                {
+                    reject(err);
+                }
+                else
+                {
+                    resolve(spreadsheet);
+                }
+            }
+        );
+    });
+}
+
+function getSheetRange (client, sheet, range)
+{
+    return new Promise((resolve, reject) => {
+        sheet.cells(
+            {range: range},
+            (err, data) => {
+                if (err)
+                {
+                    reject(err);
+                }
+                else
+                {
+                    resolve(data.cells);
+                }
+            }
+        );
+    });
+}
+
+function getLunchesFromSheet ()
+{
+    return connectToDrive(oauth2Client)
+        .then(connectToSpreadsheets)
+        .then((spreadsheet) => {
+            let getLunchSheetRange = getSheetRange.bind(
+                null,
+                oauth2Client,
+                spreadsheet.worksheets[config.lunchSheetIndex]
+            );
+
+            return getLunchSheetRange(config.lunchCellsNicksRange)
+                .then((cells) => {
+                    let nicks = new Map();
+
+                    for (let row in cells)
+                    {
+                        for (let col in cells[row])
+                        {
+                            let nick = cells[row][col].value;
+                            if (nick)
+                            {
+                                nicks.set(col, nick);
+                            }
+                        }
+                        // should be just one, so ignore the rest
+                        break;
+                    }
+
+                    console.log('Got ' + nicks.size + ' nick names from lunch spreadsheet.');
+                    return nicks;
+                })
+                .then((nicks) => {
+                    return getLunchSheetRange(config.lunchCellsRange)
+                        .then(lunchCells => {
+                            return [nicks, lunchCells];
+                        });
+                })
+                .then((nicksAndLunchesCells) => {
+                    let row;
+                    let nicks = nicksAndLunchesCells[0];
+                    let lunchCells = nicksAndLunchesCells[1];
+                    let lunches = new Map();
+
+                    for (row in lunchCells)
+                    {
+                        break;
+                    }
+                    row = Number(row) + dayOfWeekIdx;
+
+                    for (let col of nicks.keys())
+                    {
+                        let lunch = lunchCells[row][col].value;
+                        if ('-' === lunch || !lunch)
+                        {
+                            lunch = null;
+                        }
+                        lunches.set(nicks.get(col), lunch);
+                    }
+
+                    console.log('Got ' + lunches.size + ' lunches from lunch spreadsheet.');
+                    return lunches;
+                });
         });
-        callback();
+}
+
+function getLunches ()
+{
+    if (lunchCache.timestamp > Date.now() - 5 * 60)
+    {
+        return Promise.resolve(lunchCache.items);
     }
     else
     {
-        url = client.generateAuthUrl({
-            /* eslint-disable camelcase */
-            access_type: 'offline',
-            scope: [ 'https://www.googleapis.com/auth/drive' ]
-            /* eslint-enable camelcase */
-        });
-        console.log('1. Visit the url:', url);
-        console.log('2. You will end up at ' + config.oauthRedirectUrl + '?code=<CODE>#');
-        rl.question('Paste the code here: ', (code) => {
-            rl.close();
-            client.getToken(code, (err, tokens) => {
-                if (err) { throw err; }
-                client.setCredentials(tokens);
-                config.oauth2Token = tokens.access_token;
-                fs.writeFile(
-                    './config.json',
-                    JSON.stringify(config, false, 4)
-                );
-                callback();
+        return getLunchesFromSheet()
+            .then((lunches) => {
+                lunchCache.items = lunches;
+                lunchCache.timestamp = Date.now();
+                return lunches;
             });
-        });
     }
+}
+
+function getLunch (nick)
+{
+    return getLunches().then((lunches) => {
+        let lunch = lunches.get(nick);
+        console.log(nick, lunch);
+        if (!lunch)
+        {
+            throw undefined;
+        }
+        return lunch;
+    });
 }
 
 
@@ -72,14 +216,12 @@ slack.on('message', event => {
     var isFemale;
     var message = 'message' === event.type && event.text;
     var response;
-    var lunch;
 
     console.log('Got', message);
 
     user = slack.getUserByID(event.user);
     channel = slack.getChannelGroupOrDMByID(event.channel);
 
-    lunch = lunches.items.get(user.name);
     response = '@' + user.name + ': ';
 
 
@@ -87,43 +229,48 @@ slack.on('message', event => {
     {
         isFemale = user.name.split('.')[0].slice(-1);
 
-        if (lunch === undefined)
-        {
-            channel.send(response + 'Nie wiem kim jesteś, sorry.');
-        }
-        else if (!lunch || '-' === lunch)
-        {
-            if (isFemale)
-            {
-                channel.send(response + 'Wygląda na to, że nic dzisiaj nie zamówiłaś');
-            }
-            else
-            {
-                channel.send(response + 'Wygląda na to, że nic dzisiaj nie zamówiłeś');
-            }
-        }
-        else
-        {
-            channel.send(response + lunch);
-        }
+        getLunch(user.name)
+            .then(lunch => {
+                if (!lunch)
+                {
+                    if (isFemale)
+                    {
+                        channel.send(response + 'Wygląda na to, że nic dzisiaj nie zamówiłaś');
+                    }
+                    else
+                    {
+                        channel.send(response + 'Wygląda na to, że nic dzisiaj nie zamówiłeś');
+                    }
+                }
+                else
+                {
+                    channel.send(response + lunch);
+                }
+            })
+            .catch((error) => {
+                console.log(error.stack);
+                channel.send(response + 'Nie wiem kim jesteś, sorry.');
+            });
     }
 
     if (/what('s|is) for (dinner|lunch)/i.test(message))
     {
         isFemale = user.name.split('.')[0].slice(-1);
 
-        if (lunch === undefined)
-        {
-            channel.send(response + "I don't know who you are, sorry.");
-        }
-        else if (!lunch || '-' === lunch)
-        {
-            channel.send(response + 'It seems that you did not order anything today.');
-        }
-        else
-        {
-            channel.send(response + lunch);
-        }
+        getLunch(user.name)
+            .then(lunch => {
+                if (!lunch)
+                {
+                    channel.send(response + 'It seems that you did not order anything today.');
+                }
+                else
+                {
+                    channel.send(response + lunch);
+                }
+            })
+            .catch(() => {
+                channel.send(response + "I don't know who you are, sorry.");
+            });
     }
 });
 
@@ -144,72 +291,5 @@ slack.on('error', error => {
     console.log(error);
 });
 
-getOAuth2Token(oauth2Client, () => {
-    googleSpreadsheets({
-        key: config.lunchSpreadsheetId,
-        auth: oauth2Client
-    }, (googleSpreadsheetsErr, spreadsheet) => {
-        var sheet;
-        if (googleSpreadsheetsErr)
-        {
-            throw googleSpreadsheetsErr;
-        }
-        sheet = spreadsheet.worksheets[config.lunchSheetIndex];
-
-        sheet.cells({
-            range: config.lunchCellsNicksRange
-        }, (nicksErr, nicksData) => {
-            let nicks = new Map();
-            if (nicksErr)
-            {
-                throw nicksErr;
-            }
-            console.log('Got nick names from lunch spreadsheet.');
-
-            for (let row in nicksData.cells)
-            {
-                for (let col in nicksData.cells[row])
-                {
-                    let nick = nicksData.cells[row][col].value;
-                    if (nick)
-                    {
-                        nicks.set(col, nick);
-                    }
-                }
-                // should be just one, so ignore the rest
-                break;
-            }
-
-            sheet.cells(
-                {range: config.lunchCellsRange},
-                (lunchesErr, lunchesData) => {
-                    var row;
-                    if (lunchesErr)
-                    {
-                        throw lunchesErr;
-                    }
-                    console.log('Got lunches from lunch spreadsheet.');
-
-                    for (row in lunchesData.cells)
-                    {
-                        break;
-                    }
-                    row = Number(row) + dayOfWeekIdx;
-
-                    lunches.items.clear();
-                    lunches.timestamp = Date.now();
-                    for (let col of nicks.keys())
-                    {
-                        lunches.items.set(
-                            nicks.get(col),
-                            lunchesData.cells[row][col].value
-                        );
-                    }
-
-                    slack.login();
-                }
-            );
-        });
-
-    });
-});
+slack.login();
+getLunches();
