293eeabec4bb4c07a52a5071fdba28dd4f6a2385
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2015-11-05 21:45:52 +0100

    Doh, Date.now() returns seconds

--- plugins/lunch.js
+++ plugins/lunch.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const REFRESH_RATE_MS = 5 * 60 * 60 * 1000;
+const REFRESH_RATE_S = 5 * 60 * 60;
 
 const fs = require('fs');
 const readline = require('readline');
@@ -257,7 +257,7 @@ function getLunchesFromSheet ()
 
 function getLunches ()
 {
-    if (lunchCache.timestamp > Date.now() - REFRESH_RATE_MS)
+    if (lunchCache.timestamp > Date.now() - REFRESH_RATE_S)
     {
         return Promise.resolve(lunchCache.items);
     }
