1361ee48f654ea90e0a6ea8ce92544e38eaf0bb1
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2018-08-12 15:39:31 +0200

    Optional PM2 deploy config in separate file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,21 @@
+let deployConfig;
+try
+{
+    // eslint-disable-next-line global-require
+    deployConfig = require('./ecosystem.config.deploy.js');
+}
+catch (error)
+{
+    if (error instanceof Error && error.code === 'MODULE_NOT_FOUND')
+    {
+        deployConfig = {};
+    }
+    else
+    {
+        throw error;
+    }
+}
+
 module.exports = {
     apps: [
         {
@@ -5,5 +23,6 @@ module.exports = {
             script: 'index.js',
             watch: true
         }
-    ]
+    ],
+    deploy: deployConfig,
 };
